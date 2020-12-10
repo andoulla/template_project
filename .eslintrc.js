a3ada0ea354be95c5d1e680542a6b721df09e1a0
andoulla <m.a.stylianou@hotmail.com>
2020-12-10 12:09:53 +0200

    Completed linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,42 @@
 module.exports = {
-    extends: [
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:prettier/recommended',
-      'plugin:import-order/recommended',
-      'plugin:jsx-a11y/recommended'
-    ],
-    plugins: [
-        'import-order',
-        "jsx-a11y"
-    ],
-    env: {
-      es6: true,
-      node: true,
-      browser: true,
-    },
-    parserOptions: {
-      ecmaVersion: 8,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    ignorePatterns: ['/node_modules/**', '/build/**', '/src/reportWebVitals.js'],
-    rules: {
-      'no-unused-vars': ['warn', { args: 'none', argsIgnorePattern: 'req|res|next|val' }],
-      'prettier/prettier': ['error'],
-      'import-order/import-order': [ 2, {
-          'newlines-between': 'always', 
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true 
-            }
-        }]
-    },  settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:prettier/recommended',
+		'plugin:import-order/recommended',
+		'plugin:jsx-a11y/recommended',
+	],
+	plugins: ['import-order', 'jsx-a11y'],
+	env: {
+		es6: true,
+		node: true,
+		browser: true,
+	},
+	parserOptions: {
+		ecmaVersion: 8,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	ignorePatterns: ['/node_modules/**', '/build/**', '/src/reportWebVitals.js'],
+	rules: {
+		'no-unused-vars': ['warn', { args: 'none', argsIgnorePattern: 'req|res|next|val' }],
+		'prettier/prettier': ['error'],
+		'import-order/import-order': [
+			2,
+			{
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
